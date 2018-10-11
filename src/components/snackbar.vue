392fb0f2ce1c060a33c94f162fba579bbb1b653a
tuanmjnh <minhtuan200990@gmail.com>
2018-10-11 16:57:22 +0700

    tm

--- src/components/snackbar.vue
+++ src/components/snackbar.vue
@@ -1,55 +1,33 @@
 <template>
-  <v-snackbar v-model="snackbar" :bottom="y === 'bottom'" :left="x === 'left'" :multi-line="mode === 'multi-line'"
-    :right="x === 'right'" :timeout="timeout" :top="y === 'top'" :vertical="mode === 'vertical'">
-    {{ text }}
-    <v-btn color="pink" flat @click="snackbar = false"> x</v-btn>
+  <v-snackbar v-model="snackbar.show" :color="snackbar.color" :bottom="snackbar.y==='bottom'"
+    :left="snackbar.x==='left'" :multi-line="snackbar.mode==='multi-line'" :right="snackbar.x==='right'"
+    :timeout="snackbar.timeout" :top="snackbar.y==='top'" :vertical="snackbar.mode==='vertical'">
+    {{ snackbar.text }}
+    <v-btn color="" flat @click="snackbar.show=false"> <i class="material-icons">close</i></v-btn>
   </v-snackbar>
 </template>
 
 <script>
 export default {
-    data: () => {
-        return {
-            snackbar: false,
-            // y: 'top',
-            // x: 'right',
-            // mode: '',
-            // timeout: 6000,
-            // text: 'Hello, I\'m a snackbar'
-        }
-    },
+    data: () => ({
+        // snackbar: false,
+        // y: 'top',
+        // x: 'right',
+        // mode: '',
+        // timeout: 6000,
+        // text: 'Hello, I\'m a snackbar'
+    }),
     watch: {
         // show(val) {
         //     console.log(this.$store.state._message.show)
-
         //     // this.$store.dispatch('messageClose', val)
         // }
     },
     computed: {
-        show() {
-            var x = this.$store.state._message.show
+        snackbar() {
+            var x = this.$store.state._message
             return x
-        },
-        text() {
-            var x = this.$store.state._message.text
-            return x
-        },
-        mode() {
-            var x = this.$store.state._message.mode
-            return x
-        },
-        timeout() {
-            var x = this.$store.state._message.timeout
-            return x
-        },
-        x() {
-            var x = this.$store.state._message.x
-            return x
-        },
-        y() {
-            var x = this.$store.state._message.y
-            return x
-        },
+        }
     }
 }
 </script>
